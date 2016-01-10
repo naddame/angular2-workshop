5cd0a9b9751d9d47b52e4018966957de28dcc13f
nir kaufman <nirkaufman@gmail.com>
2016-01-10 15:35:52 +0200

    item class

--- src/services/item.ts
+++ src/services/item.ts
@@ -1,10 +1,10 @@
 export class Item {
 
-  public price: number;
-  public label: string;
+  public price:number;
+  public label:string;
 
   constructor(label:string, price:number) {
-
+    this.label = label;
+    this.price = price;
   }
-
 }
