8c2bbee85d232a2542ed2b5bb2023ecd3ef5a853
nir kaufman <nirkaufman@gmail.com>
2016-01-10 15:55:36 +0200

    formatting

--- src/services/order.ts
+++ src/services/order.ts
@@ -46,13 +46,11 @@ export class Order {
   }
 
   static nextId():string {
-
     function s4() {
       return Math.floor((1 + Math.random()) * 0x10000)
           .toString(16)
           .substring(1);
     }
-
     return `${s4()}${s4()}-${s4()}-${s4()}-${s4()}-${s4()}${s4()}${s4()}`;
   }
 }
